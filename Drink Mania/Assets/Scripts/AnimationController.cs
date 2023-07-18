ea1cf6a5cd790d0df9e4e0402e2e3c6317228969
SokLink <137996008+SokLink@users.noreply.github.com>
2023-07-18 17:48:40 +0300

    АХАХАХ-ААХАХХАХАХАХПХАХПХАХХ

--- Drink Mania/Assets/Scripts/AnimationController.cs
+++ Drink Mania/Assets/Scripts/AnimationController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     [SerializeField] private Animator antagonistAnimations;
-     
+    [SerializeField] private Animator antagonistAlcoholAnimations;
+
+    [SerializeField] private ParticleSystem enemyDrinkParticleSystem;
+
     public void SwapAnimation(string animName)
     {
         antagonistAnimations.Play("Idle");
+        antagonistAlcoholAnimations.Play("DworfDontDrink");
         StopAllCoroutines();
+
+        enemyDrinkParticleSystem.emissionRate = 0f;
         antagonistAnimations.Play(animName);
         StartCoroutine(StopAnimation());
     }
@@ -16,6 +22,17 @@ public class AnimationController : MonoBehaviour
     private IEnumerator StopAnimation()
     {
         yield return new WaitForSeconds(2f);
+
         antagonistAnimations.Play("Idle");
+        antagonistAlcoholAnimations.Play("DrinkDwor");
+
+        StartCoroutine(EnemyDrinkParticles());
+    }
+
+    private IEnumerator EnemyDrinkParticles()
+    {
+        yield return new WaitForSeconds(0.4f);
+
+        enemyDrinkParticleSystem.emissionRate = 100f;
     }
 }
--- Drink Mania/Assets/Scripts/DrinkParticles.cs
+++ Drink Mania/Assets/Scripts/DrinkParticles.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class DrinkParticles : MonoBehaviour
 {
-    [SerializeField] private GameObject drinkParticlesGO;
-    [SerializeField] private ParticleSystem drinkParticleSystem;
+    [SerializeField] private ParticleSystem playerDrinkParticleSystem;
 
     public void EnableAndDisableDrinkParticles(int rate)
     {
-        drinkParticleSystem.emissionRate = rate;
+        playerDrinkParticleSystem.emissionRate = rate;
     }
 
     public void SwapDrinkParticles(int nextLvl)
@@ -15,19 +14,19 @@ public class DrinkParticles : MonoBehaviour
         switch (nextLvl)
         {
             case 11:
-                drinkParticleSystem.startColor = new Color(0.65f, 0.25f, 0.13f);
+                playerDrinkParticleSystem.startColor = new Color(0.65f, 0.25f, 0.13f);
                 break;
 
             case 21:
-                drinkParticleSystem.startColor = new Color(0.25f, 0.1f, 0f);
+                playerDrinkParticleSystem.startColor = new Color(0.25f, 0.1f, 0f);
                 break;
 
             case 31:
-                drinkParticleSystem.startColor = new Color(0.75f, 0.95f, 1f, 0.1f);
+                playerDrinkParticleSystem.startColor = new Color(0.75f, 0.95f, 1f, 0.1f);
                 break;
 
             case 41:
-                drinkParticleSystem.startColor = new Color(0.75f, 0.95f, 1f, 0.1f);
+                playerDrinkParticleSystem.startColor = new Color(0.75f, 0.95f, 1f, 0.1f);
                 break;
         }
     }
